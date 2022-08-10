b4b6cabd745e10db824c850448b206401af56c93
ilham fathullah <ilhamfathullah04@gmail.com>
2022-08-10 14:24:10 +0700

    update

--- models/user.go
+++ models/user.go
@@ -1,3 +1,24 @@
-// Create package models, import "time", 
-// User struct, UsersProfileResponse  struct, 
+package models
+
+// Create package models, import "time",
+import "time"
+
+// User struct, UsersProfileResponse  struct,
+type User struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name" gorm:"type: varchar(255)"`
+	Email     string    `json:"email" gorm:"type: varchar(255)"`
+	Password  string    `json:"-" gorm:"type: varchar(255)"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+}
+
 // and UsersProfileResponse TableName method here ...
+type UsersProfileResponse struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func (UsersProfileResponse) TableName() string {
+	return "users"
+}
