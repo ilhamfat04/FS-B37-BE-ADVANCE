3a8a55c66e120f2e8cf8a1b3a743549f15fd83e8
ilham fathullah <ilhamfathullah04@gmail.com>
2022-08-11 10:55:51 +0700

    update

--- pkg/middleware/auth.go
+++ pkg/middleware/auth.go
@@ -1,7 +1,39 @@
 package middleware
 
-// import required packages here ...
+import (
+	"context"
+	dto "dumbmerch/dto/result"
+	jwtToken "dumbmerch/pkg/jwt"
+	"encoding/json"
+	"net/http"
+	"strings"
+)
 
-// Declare Result struct here ...
+func Auth(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
 
-// Create Auth function here ...
+		token := r.Header.Get("Authorization")
+
+		if token == "" {
+			w.WriteHeader(http.StatusUnauthorized)
+			response := dto.ErrorResult{Code: http.StatusBadRequest, Message: "unauthorized"}
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+
+		token = strings.Split(token, " ")[1]
+		claims, err := jwtToken.DecodeToken(token)
+
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			response := dto.ErrorResult{Code: http.StatusBadRequest, Message: "unauthorized"}
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "userInfo", claims)
+		r = r.WithContext(ctx)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
