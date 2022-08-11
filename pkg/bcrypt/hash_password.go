30764ded901883c7a5f2870c60c04e5e9c88fdcc
ilham fathullah <ilhamfathullah04@gmail.com>
2022-08-11 10:25:54 +0700

    update

--- pkg/bcrypt/hash_password.go
+++ pkg/bcrypt/hash_password.go
@@ -1,7 +1,18 @@
-// Create package bcrypt here ...
+package bcrypt
 
-// Import "golang.org/x/crypto/bcrypt" here ... 
+import "golang.org/x/crypto/bcrypt"
 
-// Create HashingPassword function here ...
+// proses register
+func HashingPassword(password string) (string, error) {
+	hashedByte, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedByte), nil
+}
 
-// Create CheckPasswordHash function here ...
+// proses login
+func CheckPasswordHash(password, hashedPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err == nil
+}
