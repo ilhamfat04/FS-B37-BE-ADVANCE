579677aba5b634a6fe3422b2eb555080e4586e5c
ilham fathullah <ilhamfathullah04@gmail.com>
2022-08-11 08:47:50 +0700

    update

--- repositories/users.go
+++ repositories/users.go
@@ -21,7 +21,7 @@ func RepositoryUser(db *gorm.DB) *repository {
 func (r *repository) FindUsers() ([]models.User, error) {
 	var users []models.User
 	// Using Preload("profile") to find data with relation to profile here ...
-	err := r.db.Preload("Profile").First(&users).Error
+	err := r.db.Preload("Profile").Find(&users).Error
 
 	return users, err
 }
